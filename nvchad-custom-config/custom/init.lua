85e0bf2942e81ea1a7d217ec1bba80f6f660857d
Vikas Kharkwal <kharkwalvikas@gmail.com>
2023-03-12 03:35:23 +0530

    update nvchad config

--- nvchad-custom-config/custom/init.lua
+++ nvchad-custom-config/custom/init.lua
@@ -1,5 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 vim.wo.relativenumber = true
+vim.opt.fileformat = "unix"
 vim.opt.shell = "pwsh.exe -NoLogo"
 vim.opt.shellcmdflag =
   "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
